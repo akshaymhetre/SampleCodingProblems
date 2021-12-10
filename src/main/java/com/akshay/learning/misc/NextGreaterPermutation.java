4840c23fcfa04d638929f9c29967c8b4feeece73
akshaymhetre <akshaymhetre1@gmail.com>
2021-12-10 12:36:53 +0530

    #Adding some thread related problems

--- src/main/java/com/akshay/learning/misc/NextGreaterPermutation.java
+++ src/main/java/com/akshay/learning/misc/NextGreaterPermutation.java
@@ -32,28 +32,28 @@ public class NextGreaterPermutation {
     * - Swap X AND Y
     * - reverse elements next to first lesser element till the end
     * */
-    public void nextPermutation(int[] nums) {
-        int i = nums.length-2;
-        while (i >= 0 && nums[i] >=nums[i+1]){
+    public int[] nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
             i--;
         }
-        // found X;
-        if(i > 0){
-            int j = nums.length-1;
-            while (nums[i] >= nums[j]){
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
                 j--;
             }
             swap(nums, i, j);
         }
-        reverse(nums, i+1);
+        reverse(nums, i + 1);
+        return nums;
     }
 
     private void reverse(int[] nums, int start) {
-        int last = nums.length-1;
-        while (start < last){
-            swap(nums, start, last);
-            start++;
-            last--;
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
         }
     }
 
